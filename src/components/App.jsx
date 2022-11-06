58b7156c0a1dfe44416b470be040ce32a0032fca
Marina <sheremetamaryna@gmail.com>
2022-11-06 23:19:07 +0100

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,74 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import ContactFilter from './ContactFilter';
+import Form from './Form';
+import { nanoid } from 'nanoid';
+import './Phonebook.css';
+import ContactList from './ContactList';
+
+export default class App extends React.Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+  onChange = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+  onSubmit = data => {
+    console.log(data);
+    const { name, number } = data;
+    console.log(
+      this.state.contacts.filter(contact => contact.name === name).length
+    );
+
+    if (
+      this.state.contacts.filter(
+        contact => contact.name.toLocaleLowerCase() === name.toLocaleLowerCase()
+      ).length === 0
+    ) {
+      const id = nanoid();
+      const contact = {
+        id: id,
+        name: name,
+        number: number,
+      };
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, contact],
+      }));
+    } else {
+      alert('This contact already exist');
+    }
+  };
+
+  onDelete = event => {
+    const id = event.target.parentElement.id;
+    this.setState({
+      contacts: this.state.contacts.filter(contact => contact.id !== id),
+    });
+  };
+  render() {
+    return (
+      <div>
+        <h1 className="Phonebook__text">Phonebook</h1>
+
+        <Form onSubmit={this.onSubmit}></Form>
+
+        <h2 className="Phonebook__text__contacts">Contacts</h2>
+
+        <ContactFilter
+          filter={this.state.filter}
+          onChange={this.onChange}
+        ></ContactFilter>
+        <ContactList
+          contacts={this.state.contacts}
+          filter={this.state.filter}
+          onDelete={this.onDelete}
+        ></ContactList>
+      </div>
+    );
+  }
+}
